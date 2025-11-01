472bbf07ada7bc0c7c0b8dfc638e03a862095a42
Rabina22272810 <22272810@student.curtin.edu.au>
2025-11-01 08:11:59 +0000

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('My Student ID is 22272810. Welcome to Express');
+  res.render('index', {title: 'World' });
 });
 
 export default router;
